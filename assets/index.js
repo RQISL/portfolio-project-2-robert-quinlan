337354f452d6b88c3697abbb01c8c797d4bd3d9a
Robert Quinlan <creativewb50@gmail.com>
2023-02-08 09:39:40 +0000

    create select one image as test

--- assets/index.js
+++ assets/index.js
@@ -8,19 +8,25 @@ const options = {
     }
 };
 
-fetch('https://imdb-top-100-movies.p.rapidapi.com/', options)
-    .then(response => response.json())
+// fetch('https://imdb-top-100-movies.p.rapidapi.com/', options)
+//     .then(response => response.json())
 
-    .then(image => {
-        const list = image;
+//     .then(image => {
+//         const list = image;
 
-        list.map(item => {
-            const poster = item.image;
-            const movie = `<img src="${poster}">`;
-            document.querySelector('.movies').innerHTML += movie;
-            console.log(item);
+//         list.map(item => {
+//             const poster = item.image;
+//             //const movie = `<img src="${poster}">`;
+//            // document.querySelector('.movies').innerHTML += movie;
+//             console.log(item);
 
-        })
+//         })
 
-    })
-    .catch(err => console.error(err));
+//     })
+//     .catch(err => console.error(err));
+
+let arrayOfImages = [];
+
+arrayOfImages.push(['https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL75_UX380_CR0,1,380,562_.jpg']);
+const movie = `<img src="${arrayOfImages}">`;
+document.querySelector('.movies').innerHTML = movie;
