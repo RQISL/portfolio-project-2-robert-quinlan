0fda54d415ca5edf5641f4d3089ac81e3ce8b637
RQISL <creativewb50@gmail.com>
2023-03-17 16:06:57 +0000

    Delete entername.js
    It does not use it anymore

--- assets/entername.js
+++
@@ -1,7 +0,0 @@
-// For index.html javascript
-function entergame() {
-    let enterName = document.getElementById('#username').value;
-    let displayname = document.getElementById('#displayName').style.display = 'block';
-    displayname.innerHTML = localStorage.getItem("enterName");
-}
-//console.log(displayname, "show name");
