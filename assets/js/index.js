08fcc6776b64b132b5f882608992a52f2d881c12
Robert Quinlan <creativewb50@gmail.com>
2024-01-06 20:40:26 +0000

    modified javasript and style

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,87 +1,123 @@
-// Enter username its disable unless user must fill enter the name.
-let username = document.querySelector('#username');
-let UserName = document.querySelector('#enterUsername');
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
-username.addEventListener('keyup', () => {
-    UserName.disabled = !username.value;
-});
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
 
-//  Output username on the screen after enter username and disappear the screen of enter username. 
-function entergame() {
-    let displayname = document.querySelector('#displayName');
-    let username = document.querySelector('#username').value;
-    displayname.innerHTML = `<span>Welcome</span> <span id="colorName"> ` + username + `</span><span> Movie Quiz</span>`;
-    document.getElementById("hidden").style.visibility = "hidden";
-}
 
-// Rapid Api key
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '8155640071msh82e17e6cc71dfcep12441bjsna2ec5fe1f5c1',
-        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
-    }
+// movie game javascript
+
+fetch(
+  'questions.json'
+)
+  .then((res) => {
+      return res.json();
+  })
+  
+  .then((loadedQuestions) => {
+    
+      poster = loadedQuestions.results.map((moviePoster) => {
+        
+        const imageMovie = {image: moviePoster.image.imageUrl};
+        const posters =`<img src="${imageMovie}">`;
+        
+        document.querySelector(".images").innerHTML = posters;
+
+        
+      })
+
+
+      questions = loadedQuestions.results.map((loadedQuestion) => {
+          const formattedQuestion = {
+              question: loadedQuestion.question,
+          };
+
+          const answerChoices = [...loadedQuestion.incorrect_answers];
+          formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+          answerChoices.splice(
+              formattedQuestion.answer - 1,
+              0,
+              loadedQuestion.correct_answer
+          );
+
+          answerChoices.forEach((choice, index) => {
+              formattedQuestion['choice' + (index + 1)] = choice;
+          });
+
+          return formattedQuestion;
+      });
+      startGame();
+  })
+  .catch((err) => {
+      console.error(err);
+  });
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
 };
 
-// from Api collect poster of images in the array
-let arrayOfImages = [];
-arrayOfImages.push(
-    'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL75_UX380_CR0,1,380,562_.jpg alt = "Movie poster called The ShawsHank"',
-    'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UY562_CR8,0,380,562_.jpg alt="Movie poster called The Godfather"',
-    'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_QL75_UX380_CR0,0,380,562_.jpg alt="Movie poster called Batman, The Dark Night"',
-    'https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctZjczMi00OTk4LTg3NWUtNmVhMTAzNTNjYjcyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_QL75_UX380_CR0,16,380,562_.jpg alt="Movie poster called Seven"'
-);
 
-// the questions and answers in array.
-let myQuestion = [];
-myQuestion.push(
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    //go to the end page
+    return window.location.assign("/gameover.html");
+  }
+  questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
 
-    ` <p class="boxQ">Who was writer screenplay?</p>
-            <p class="box incorrectAns" onclick="answer_Right_or_Wrong(this)">Francis Ford Coppola</p>
-            <p class="box correctAns" onclick="answer_Right_or_Wrong(this)">Frank Darabont</p>
-            <p class="box incorrectAns" onclick="answer_Right_or_Wrong(this)">Jonathan Nolan</p>`,
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-    `<p class="boxQ">Who was in the actor?</p>
-     <p class="box incorrectAns" onclick="answer_Right_or_Wrong(this)" >Morgan Freeman</p>
-     <p class="box incorrectAns" onclick="answer_Right_or_Wrong(this)" >Tim Robbins</p>
-     <p class="box correctAns"  onclick="answer_Right_or_Wrong(this)">Marlon Brando</p>`,
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
 
-    `<p class="boxQ">Who was the director in this film?</p>
-     <p class="box correctAns" onclick="answer_Right_or_Wrong(this)" >Christopher Nolan</p>
-     <p class="box incorrectAns" onclick="answer_Right_or_Wrong(this)" >David Fincher</p>
-     <p class="box incorrectAns" onclick="answer_Right_or_Wrong(this)" >Francis Ford Coppola</p>`,
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
 
-    `<p class="boxQ">When was the release this film?</p>
-     <p class="box incorrectAns"  onclick="answer_Right_or_Wrong(this)">2008</p>
-     <p class="box correctAns"  onclick="answer_Right_or_Wrong(this)">1995</p>
-     <p class="box incorrectAns"  onclick="answer_Right_or_Wrong(this)">1994</p>`,
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
 
-);
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
-// declare variable for the function
-const firstImg = 0;
-const lastImg = arrayOfImages.length;
-let currentImg = 0;
-let poster = document.getElementsByClassName('poster')[0];
-let questions = 0;
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-// declared variable for the button click next the slides
-const nextBtn = document.getElementById('btnNext');
-let showUp = document.getElementById('GameAgain').style.display = "none";
-nextBtn.addEventListener('click', () => {
-    currentImg++;
-    if (currentImg >= lastImg) {
-        currentImg = lastImg; // hit to last number of 4
-        nextBtn.disabled = lastImg; // clcik is stop when reach to 4
-        showUp = document.getElementById('GameAgain').style.display = "block";
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
     }
-    poster.src = arrayOfImages[currentImg];
-    document.getElementById('numberCounter').innerHTML = (currentImg + 1) + '/4';
-    questions = document.getElementById('questions').innerHTML = myQuestion[currentImg];
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
 });
 
-// select the answer to make highlight background color for correct or incorrect.
-function answer_Right_or_Wrong(Ans) {
-    Ans.classList.toggle("active");
-}
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
 
